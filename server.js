e881fa3aad0aacd2477250114f15ff4c24fa0731
k8 <katesterchi@gmail.com>
2023-10-23 20:33:07 -0500

    created file structure and began coding entry point with server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(bodyParser.json());
+
+// MongoDB connection
+
+
+// Routes
+const userRoutes = require('./routes/userRoutes');
+const thoughtRoutes = require('./routes/thoughtRoutes');
+const friendRoutes = require('./routes/friendRoutes');
+
+app.use('/api/users', userRoutes);
+app.use('/api/thoughts', thoughtRoutes);
+app.use('/api/friends', friendRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
